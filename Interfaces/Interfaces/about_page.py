f79cda89e5fc26c54f3a13603ce192303ce7cb8f
JessyLopM06 <153002429+JessyLopM06@users.noreply.github.com>
2024-05-31 15:12:36 -0600

    about_page.py Cambios

--- Interfaces/Interfaces/about_page.py
+++ Interfaces/Interfaces/about_page.py
@@ -1,39 +1,81 @@
 import tkinter as tk
 
-def about_page(root):
-    about_page_fm = tk.Frame(root)
-    
-    about_page_lb = tk.Label(about_page_fm, text='About', font=('Arial', 18, 'bold'), fg='#1414b8')
-    about_page_lb.pack(pady=20)
-    
-    about_text = """
-    1. Introduction
-The research on energy storage devices such as supercapacitors and batteries generates a lot of data that must be processed manually in several software, slowing down its analysis and interpretation. Thus, the lack of specialized software hinders accurate data analysis in the field of  supercapacitors, especially for non-programming users.
-In this work we report a new software designed to simplify the electrochemical data analysis obtained in the study of new materials and devices for electrochemical capacitors and batteries. The Multiple Analyses of Supercapacitors Software (MASC) offers an efficient solution for electrochemical data analysis in this growing field.
-
-2. Methodology​
-MASC is purpose-built software for simplifying electrochemical data analysis in the quest for new supercapacitor materials and devices. Developed in Python with tkinter, MASC efficiently executes complex calculations using various functions.
-Functionality: MASC accurately computes electrochemical properties, ensuring comprehensive coverage. Testing: Stringent testing protocols validate its accuracy and reliability across scenarios.
-Structured in three modules, MASC seamlessly integrates data processing workflows, utilizing CSV files for input. Its goal is to offer an intuitive interface for researchers and scientists.
-MASC aims to enhance data analysis efficiency by providing specialized and intuitive software. Initially targeted at CIIDIT's supercapacitor lab, future iterations will cater to users with diverse experience levels.
-Requirements Analysis: Comprehensive understanding of project needs is key. Software Design: Focus on creating an intuitive and functional interface. Functionality Implementation: Robust algorithms for precise calculations. Testing and Validation: Extensive testing ensures reliability. Implementation and Distribution: Initial deployment planned in the research lab, with broader accessibility in future phases.​
-    """
-
-    about_text_txt = tk.Text(about_page_fm, wrap="word")
-    about_text_txt.insert(tk.END, about_text)
-    about_text_txt.config(state="disabled", font=("Arial", 11), bg="gray84", fg="black")
-    about_text_txt.pack(expand=True, fill="both", padx=60, pady=10)
-
+def about_page():
+    global image_left, image_center, image_right  # Hacer las variables de imagen globales
+    about_page_fm = tk.Frame(main_fm, bg='gray84')  # Cambiar el fondo del marco a gris
     about_page_fm.pack(fill=tk.BOTH, expand=True)
 
-# Crear la ventana principal
-root = tk.Tk()
-root.title("About Page")
+    about_page_lb = tk.Label(about_page_fm, text='About MASSC: Multiple Analyses Software for Supercapacitors',
+                             font=('Arial', 15, 'bold'), fg='#1414b8', bg='gray84')
+    about_page_lb.pack(pady=10, padx=10)
 
-root.geometry("800x600")
+    # Crear un marco para contener las imágenes
+    image_frame = tk.Frame(about_page_fm, bg='gray84')
+    image_frame.pack(side=tk.TOP, fill=tk.X)
+
+    # Configurar columnas para que se expandan y centren las imágenes
+    image_frame.columnconfigure(0, weight=1)
+    image_frame.columnconfigure(1, weight=1)
+    image_frame.columnconfigure(2, weight=1)
+
+    # Cargar las imágenes
+    image_left = tk.PhotoImage(file="imagenes/uniM.png")
+    label_left = tk.Label(image_frame, image=image_left, bg='gray84')
+    label_left.grid(row=0, column=0, padx=10)
+
+    image_center = tk.PhotoImage(file="imagenes/logM1.png")  # m2ph
+    label_center = tk.Label(image_frame, image=image_center, bg='gray84')
+    label_center.grid(row=0, column=1)
+
+    image_right = tk.PhotoImage(file="imagenes/92.png")
+    label_right = tk.Label(image_frame, image=image_right, bg='gray84')
+    label_right.grid(row=0, column=2, padx=10)
+
+    texto_copyright = "Copyright © rlucioporto.com\nLa ciencia, una luz en la oscuridad.\n   "
+    label_copyright = tk.Label(about_page_fm, text=texto_copyright, bg="navy", fg="white", font=("Arial", 8, 'bold'))
+    label_copyright.pack(side="bottom", fill="both")
+
+    # Texto debajo de las imágenes
+    texto = "Dr. Raúl Lucio Porto\nCentro de Innovación, Investigación y Desarrollo en Ingeniería y Tecnología\nCentro de Innovación en Ingeniería de Tecnología Inteligente Biomédica y Mecatrónica\n"
+    label_texto = tk.Label(about_page_fm, text=texto, justify="center", wraplength=1000, bg="blue4", fg="white", font=("Arial", 9, 'bold'))
+    label_texto.pack(fill="both", expand=True, padx=10, pady=10)
+
+    about_text = (
+        "Welcome to MASSC (Multiple Analyses Software for Supercapacitors), the ultimate software solution for advanced "
+        "electrochemical analysis of supercapacitors. Designed with researchers and engineers in mind, our platform offers "
+        "a comprehensive suite of tools for conducting cyclic voltammetry, galvanostatic charge-discharge, chronoamperometry, "
+        "electrochemical impedance spectroscopy analyses, and more. With intuitive data visualization, robust analytical "
+        "capabilities, and seamless integration with your laboratory equipment, MASSC empowers you to gain deeper insights "
+        "into the performance and optimization of supercapacitors. Join a community of innovators who are pushing the boundaries "
+        "of energy storage technology with the precision and reliability that only MASSC can provide.\n\n"
+        "MASSC was designed in the Facultad de Ingeniería Mecánica y Eléctrica from the Universidad Autónoma de Nuevo León. "
+        "If you are going to use MASSC, please cite the following two scientific articles.\n\n"
+        "◾Layered Vanadium Phosphates as Electrodes for Electrochemical Capacitors Part I: The Case of VOPO₄·2H₂O. Journal of "
+        "The Electrochemical Society, 2021, 168, 070531.DOI 10.1149/1945-7111/ac11a3.\n"
+        "◾Layered Vanadium Phosphates as Electrodes for Electrochemical Capacitors Part II: The Case of VOPO₄·CTAB and "
+        "K₀.₅VOPO₄·1.5H₂O. Journal of The Electrochemical Society, 2021, 168, 090520.DOI 10.1149/1945-7111/ac23a0."
+    )
+    about_text_txt = tk.Text(about_page_fm, wrap="word")
+    about_text_txt.insert(tk.END, about_text)
+    about_text_txt.config(state="disabled", font=("Arial", 12), bg="gray84", fg="black")
+    about_text_txt.pack(expand=True, fill="both", padx=60, pady=10)
+    
+    about_page_fm.pack(fill=tk.BOTH, expand=True)
+
+# Crear la ventana principal de Tkinter
+root = tk.Tk()
+
+ruta_icono = "./imagenes/wh.ico"
+root.iconbitmap(ruta_icono)
+
+root.title("MASSC - About Page")
+
+# Crear el marco principal
+main_fm = tk.Frame(root, bg='gray84')  # Cambiar el fondo del marco principal a gris
+main_fm.pack(fill=tk.BOTH, expand=True)
 
 # Llamar a la función para mostrar la página "About"
-about_page(root)
+about_page()
 
-# Iniciar el bucle principal de la aplicación
+# Iniciar el bucle principal de Tkinter
 root.mainloop()
